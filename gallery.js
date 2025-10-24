e0f37c4115bba681f20077b2194f43d80b221931
Hrisikeshhh <hrisikeshgogoi09@gmail.com>
2025-10-24 20:15:23 +0530

    add

--- gallery.js
+++ gallery.js
@@ -21,3 +21,40 @@ function openImg(img) {
        document.querySelector("#pic2").src = pics.image2;
        document.querySelector("#pic3").src = pics.image3;
     })
+
+
+
+
+          // mobile
+let mobile = gsap.matchMedia();
+
+mobile.add("(max-width:800px)",()=>{
+let menu = document.querySelector("#menu");
+let close = document.querySelector("#close");
+let tl1 = gsap.timeline();
+
+let tl = gsap.timeline();
+tl.to(".navigationItems",{
+   x :"100vw",
+   duration:0.4
+})
+
+
+
+tl.from(".navItem",{
+   x:30,
+   opacity:0,
+   stagger:0.2
+})
+
+tl.pause();
+
+menu.addEventListener("click",()=>{
+    tl.play();
+})
+
+close.addEventListener("click",()=>{
+    tl.reverse();
+})
+
+});
