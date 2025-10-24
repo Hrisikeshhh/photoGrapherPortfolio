e2cc6f7219fcbe1333948e916417fa20a7c5d9d8
Hrisikeshhh <hrisikeshgogoi09@gmail.com>
2025-10-24 19:55:36 +0530

    add

--- abum.js
+++ abum.js
@@ -27,3 +27,37 @@ fifthDiv.addEventListener("click",()=>{
     window.location.href = "creative&specialty.html";
 })
 })
+
+  // mobile
+let mobile = gsap.matchMedia();
+
+mobile.add("(max-width:800px)",()=>{
+let menu = document.querySelector("#menu");
+let close = document.querySelector("#close");
+let tl1 = gsap.timeline();
+
+let tl = gsap.timeline();
+tl.to(".navigationItems",{
+   x :"100vw",
+   duration:0.4
+})
+
+
+
+tl.from(".navItem",{
+   x:30,
+   opacity:0,
+   stagger:0.2
+})
+
+tl.pause();
+
+menu.addEventListener("click",()=>{
+    tl.play();
+})
+
+close.addEventListener("click",()=>{
+    tl.reverse();
+})
+
+})
